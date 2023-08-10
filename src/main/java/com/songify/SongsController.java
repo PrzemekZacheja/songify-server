5539acdc8a94b9f517235873b83ff814cc50f5e8
Przemysław Zacheja <przemek.zacheja@gmail.com>
2023-08-10 20:52:09 +0200

    added Lombok, added RequestParam to "/songs", added RequestHeader to "songs/{id}"

--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -1,39 +1,44 @@
 package com.songify;
 
+import lombok.extern.log4j.*;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @RestController
+@Log4j2
 public class SongsController {
 
     Map<Integer, SongDto> databaseInMemory = new HashMap<>();
 
     @GetMapping("/songs")
-    public ResponseEntity<SongResponseDto> getAllSongs() {
+    public ResponseEntity<SongResponseDto> getAllSongs(@RequestParam(required = false) Integer id) {
         databaseInMemory.put(1, new SongDto("Song1"));
         databaseInMemory.put(2, new SongDto("Song2"));
         databaseInMemory.put(3, new SongDto("Song3"));
-        SongResponseDto songResponseDto = new SongResponseDto(databaseInMemory);
-        return ResponseEntity.ok(songResponseDto);
+        if (id != null) {
+            SongDto songDto = databaseInMemory.get(id);
+            if (songDto == null) {
+                return ResponseEntity.notFound()
+                                     .build();
+            }
+            SongResponseDto singleMapOfSongDto = new SongResponseDto(Map.of(id, songDto));
+            return ResponseEntity.ok(singleMapOfSongDto);
+        }
+        return ResponseEntity.ok(new SongResponseDto(databaseInMemory));
     }
 
     @GetMapping("/songs/{id}")
-    public ResponseEntity<SongDto> getSongById(@PathVariable("id") int id) {
+    public ResponseEntity<SongDto> getSongById(@PathVariable("id") int id,
+                                               @RequestHeader String  authorizationHeader) {
+        log.info("Authorization header: {}", authorizationHeader);
         SongDto songDto = databaseInMemory.get(id);
-        if (songDto == null){
-            return ResponseEntity.notFound().build();
+        if (songDto == null) {
+            return ResponseEntity.notFound()
+                                 .build();
         }
         return ResponseEntity.ok(songDto);
     }
 
-    @GetMapping("/songs/")
-    public ResponseEntity<SongDto> getSongByIdRequestParam(@RequestParam("id") Integer id) {
-        SongDto songDto = databaseInMemory.get(id);
-        if (songDto == null){
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(songDto);
-    }
 }

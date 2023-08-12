669f94e15c996cfdf40a9ef000d8c6d793c60a38
Przemysław Zacheja <przemek.zacheja@gmail.com>
2023-08-12 15:51:34 +0200

    added thymeleaf and add simple index.html and songs.html, refactor SongsController

--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -1,27 +1,31 @@
 package com.songify;
 
-import lombok.extern.log4j.*;
-import org.springframework.http.*;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @Log4j2
 public class SongsController {
 
-    Map<Integer, SongDto> databaseInMemory = new HashMap<>();
+    Map<Integer, SongDto> databaseInMemory = new HashMap<>(Map.of(
+            1, new SongDto("Song1"),
+            2, new SongDto("Song2"),
+            3, new SongDto("Song3"),
+            4, new SongDto("Song4")
+    ));
 
     @GetMapping("/songs")
     public ResponseEntity<SongResponseDto> getAllSongs(@RequestParam(required = false) Integer id) {
-        databaseInMemory.put(1, new SongDto("Song1"));
-        databaseInMemory.put(2, new SongDto("Song2"));
-        databaseInMemory.put(3, new SongDto("Song3"));
+
         if (id != null) {
             SongDto songDto = databaseInMemory.get(id);
             if (songDto == null) {
                 return ResponseEntity.notFound()
-                                     .build();
+                        .build();
             }
             SongResponseDto singleMapOfSongDto = new SongResponseDto(Map.of(id, songDto));
             return ResponseEntity.ok(singleMapOfSongDto);
@@ -30,13 +34,14 @@ public class SongsController {
     }
 
     @GetMapping("/songs/{id}")
-    public ResponseEntity<SongDto> getSongById(@PathVariable("id") int id,
-                                               @RequestHeader String  authorizationHeader) {
-        log.info("Authorization header: {}", authorizationHeader);
+    public ResponseEntity<SongDto> getSongById(@PathVariable("id") int id, @RequestHeader(required = false) String authorizationHeader) {
+        if (authorizationHeader != null) {
+            log.info("Authorization header: {}", authorizationHeader);
+        }
         SongDto songDto = databaseInMemory.get(id);
         if (songDto == null) {
             return ResponseEntity.notFound()
-                                 .build();
+                    .build();
         }
         return ResponseEntity.ok(songDto);
     }
---
+++ src/main/java/com/songify/SongsViewController.java
@@ -0,0 +1,29 @@
+package com.songify;
+
+import org.springframework.stereotype.*;
+import org.springframework.ui.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@Controller
+public class SongsViewController {
+
+    Map<Integer, SongDto> databaseInMemory = new HashMap<>();
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/views/songs")
+    public String songs(Model model) {
+        databaseInMemory.put(1, new SongDto("Song1"));
+        databaseInMemory.put(2, new SongDto("Song2"));
+        databaseInMemory.put(3, new SongDto("Song3"));
+
+        model.addAttribute("songs", databaseInMemory);
+        return "songs";
+    }
+
+}
